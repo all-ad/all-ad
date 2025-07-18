c443049fb0f50abfc247c3d6c90d418c891d66ff
jjjjun1014 <152350106+jjjjun1014@users.noreply.github.com>
2025-07-19 00:38:45 +0900

    feat: Add Facebook SDK for Meta Ads integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Providers } from "./providers";
 
 import { siteConfig } from "@/config/site";
 import { fontMono, nanumMyeongjo, notoSerifKr } from "@/config/fonts";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: {
@@ -118,6 +119,26 @@ export default function RootLayout({
           nanumMyeongjo.variable,
         )}
       >
+        <Script id="facebook-sdk" strategy="afterInteractive">
+          {`
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1517283656291275',
+      xfbml      : true,
+      version    : 'v23.0'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+`}
+        </Script>
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           {children}
         </Providers>
